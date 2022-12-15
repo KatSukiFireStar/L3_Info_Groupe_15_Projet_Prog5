930f26f49f0655567260410e37d74cf09b444878
Corenthin Z <38cz74@gmail.com>
2022-12-15 14:15:54 +0100

    Create source

--- elfFile.c
+++ elfFile.c
@@ -4,7 +4,26 @@
 
 #include "elfFile.h"
 
-// Todo
+void ShowSectionFromIndex(FILE *elfFile, Elf32_Shdr *table, int index)
+{
+    Elf32_Shdr section = table[index];
+
+
+}
+
+void ShowSectionFromName(FILE *elfFile, Elf32_Shdr *table, Elf32_Word name)
+{
+    size_t tableSize = sizeof(table) / sizeof(table[0]);
+
+    for (int i = 0; i < tableSize; i++)
+    {
+        if (table[i].sh_name == name)
+        {
+            ShowSectionFromIndex(elfFile, table, i);
+            return;
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
