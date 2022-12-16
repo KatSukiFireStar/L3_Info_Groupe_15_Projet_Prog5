f539a78bb4a5d4b1bc73e0858b3e8efca616f5ab
BabakarSyr <116119576+BabakarSyr@users.noreply.github.com>
2022-12-16 16:05:49 +0100

    Update elfFile.c

--- elfFile.c
+++ elfFile.c
@@ -34,6 +34,7 @@ void ShowSectionFromName(FILE *elfFile, Elf32_Shdr *table, Elf32_Ehdr header, El
     }
 }
 
+
 Elf32_Shdr *ShowSectionTableAndDetails(FILE *elfFile, Elf32_Ehdr header){
     Elf32_Shdr *sectionTable = malloc(sizeof(Elf32_Shdr) * header.e_shnum);
     fseek(elfFile, header.e_shoff, SEEK_SET);
@@ -41,109 +42,82 @@ Elf32_Shdr *ShowSectionTableAndDetails(FILE *elfFile, Elf32_Ehdr header){
     //Pour chaque entrée de la table, il est demand´e d’afficher les principales caractéristiques
     int i;
     for (i = 0; i < header.e_shnum; i++) {
-        //lire le nom de la section
+       //Le nom de la section
         fread(&sectionTable[i].sh_name, sizeof(Elf32_Word), 1, elfFile);
-        printf("Section: %ls\n", &sectionTable[i].sh_name);
+        printf("Nom de la section : %s\n", sectionTable[i].sh_name);
+
+         //Lire le type de la section
+        fread(&sectionTable[i].sh_type, sizeof(Elf32_Word), 1, elfFile);
+        printf("Nom du Type de la section : %s\n", sectionTable[i].sh_type);
+
+         //Lire l'adresse de la section
+        fread(&sectionTable[i].sh_addr, sizeof(Elf32_Addr), 1, elfFile);
+        //l'adresse à  laquelle le premier octet de la section doit se trouver.
+        printf("Adresse de la section : %x\n", sectionTable[i].sh_addr);
+        
+          //Lire la position de la section
+        fread(&sectionTable[i].sh_offset, sizeof(Elf32_Word), 1, elfFile);
+        printf("Position de la section : %x\n", sectionTable[i].sh_offset);
+
+
         //Lire la taille de la section
         fread(&sectionTable[i].sh_size, sizeof(Elf32_Word), 1, elfFile);
-        printf("Taille de la section : %d\n", sectionTable[i].sh_size);
-        //Lire le type de la section
-        fread(&sectionTable[i].sh_type, sizeof(Elf32_Word), 1, elfFile);
-        printf("Type de la section : %d\n", sectionTable[i].sh_type);
-        //Lire la position de la section
-        fread(&sectionTable[i].sh_offset, sizeof(Elf32_Word), 1, elfFile);
-        printf("Position de la section : %d\n", sectionTable[i].sh_offset);
+        printf("Taille de la section : %x\n", sectionTable[i].sh_size);
+
+         //Lire la taille de l'entrée
+        fread(&sectionTable[i].sh_entsize, sizeof(Elf32_Word), 1, elfFile);
+        //la taille de l'entrée, pour les sections qui contiennent une table d'entrées de même taille.
+        printf("La taille de l'entrée, pour les sections qui contiennent une table d'entrées de même taille : %x\n", sectionTable[i].sh_entsize);
+        
+       
+      
 
         fread(&sectionTable[i].sh_flags, sizeof( Elf32_Word), 1, elfFile);
         if ((sectionTable[i].sh_flags & SHF_WRITE) == SHF_WRITE)
-            printf("Cette section contient des données qu'il devrait être possible d'écrire durant l'exécution du processus.\n");
+        //Cette section contient des données qu'il devrait être possible d'écrire durant l'exécution du processus;
+        printf("%s",W);
                    
         if ((sectionTable[i].sh_flags & SHF_ALLOC)== SHF_ALLOC) 
-            printf("La section fait partie de l'image mémoire du programme à exécuter.\n");
+        //La section fait partie de l'image mémoire du programme à exécuter.");
+        printf("%s",A);
         
                 
         if ((sectionTable[i].sh_flags & SHF_EXECINSTR)== SHF_EXECINSTR)
-            printf("La section contient du code exécutable.\n");
+       //La section contient du code exécutable.\n");
+        printf("%s",X);
        
         if ((sectionTable[i].sh_flags & SHF_MASKPROC) == SHF_MASKPROC) 
-            printf("Tous les bits contenus dans ce masque sont réservés à des sémantiques spécifiques au processeur.\n");
-       
-        //Lire l'adresse de la section
-        fread(&sectionTable[i].sh_addr, sizeof(Elf32_Addr), 1, elfFile);
-        //l'adresse à  laquelle le premier octet de la section doit se trouver.
-        printf("Adresse de la section : %d\n", sectionTable[i].sh_addr);
+        //Tous les bits contenus dans ce masque sont réservés à des sémantiques spécifiques au processeur
+        printf("%s",M);
 
-        //Lire le décalage de la section
-        fread(&sectionTable[i].sh_offset, sizeof(Elf32_Word), 1, elfFile);
-        //le déplacement du premier octet de la section par rapport  au  début  du  fichier
-        printf("Décalage de la section : %d\n", sectionTable[i].sh_offset);
+       
+       
+        
 
         //Lire l'indice de la table des en-têtes de sections
         fread(&sectionTable[i].sh_link, sizeof(Elf32_Word), 1, elfFile);
         //lien vers un indice de la table des en-têtes de  sections,
-        printf("Lien vers un indice de la table des en-têtes de sections : %d\n", sectionTable[i].sh_link);
+        printf("Lien vers un indice de la table des en-têtes de sections : %x\n", sectionTable[i].sh_link);
 
 
         //Lire les informations supplémentaires
         fread(&sectionTable[i].sh_info, sizeof(Elf32_Word), 1, elfFile);
         //informations supplémentaires, dépendant du type de section.
-        printf("Informations supplémentaires, dépendant du type de section : %d\n", sectionTable[i].sh_info);
+        printf("Informations supplémentaires, dépendant du type de section : %x\n", sectionTable[i].sh_info);
 
         //Lire la taille de l'alignement
         fread(&sectionTable[i].sh_addralign, sizeof(Elf32_Word), 1, elfFile);
         //la taille de l'alignement, exprimée en puissance de 2.
-        printf("La taille de l'alignement, exprimée en puissance de 2 : %d\n", sectionTable[i].sh_addralign);
+        printf("La taille de l'alignement, exprimée en puissance de 2 : %x\n", sectionTable[i].sh_addralign);
 
-        //Lire la taille de l'entrée
-        fread(&sectionTable[i].sh_entsize, sizeof(Elf32_Word), 1, elfFile);
-        //la taille de l'entrée, pour les sections qui contiennent une table d'entrées de même taille.
-        printf("La taille de l'entrée, pour les sections qui contiennent une table d'entrées de même taille : %d\n", sectionTable[i].sh_entsize);
-        
+       
     }
     return sectionTable;
 }
 
-/*L’objectif de cette étape est de construire un programme capable de lire et d’afficher les tables de
-réimplantation(repositionnement) d’un fichier ELF pour machine ARM. Pour chaque entrée, il est demandé d’afficher les
-informations suivantes :
-— la cible de la réimplantation 3
-;
-— le type de réimplantation à effectuer (noter que chaque architecture mat´erielle dispose d’une
-spécification distincte pour les types de réimplantation) ;
-— l’index de l’entrée concernée dans la table des symboles (le symbole impliqu´e dans la réimplantation,
-par exemple, dans le cas d’un appel de fonction, le symbole impliqu´e est le nom de la fonction appel´ee).
-3. Le champ correspondant est nomme offset mais son contenu dépend du type de fichier ELF qui le contient. Pour un fichier
-translatable, il s’agit effectivement d’un offset à partir du début de la section concernée. Pour un fichier ELF ex´ecutable ou une
-biblioth`eque partag´ee, le champ correspond en fait à l’adresse mémoire de l’element concerné.
-*/
-/*La fonction ShowReimplantationTablesAndDetails permet d'afficher des tables de réimplantation*/
-Elf32_Rel *ShowReimplantationTablesAndDetails(FILE *elfFile, Elf32_Ehdr header){
-
-}
-
-void BackToBegin(FILE *file)
-{
+void BackToBegin(FILE *file){
     fseek(file, 0, SEEK_SET);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 int main(int argc, char *argv[])
